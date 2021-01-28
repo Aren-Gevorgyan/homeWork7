2d08bd0d1f6127fd3743e2b4ba50f992dce1e182
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-01-28 20:30:10 +0400

    sssssssssdddddd

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
-// let lockButton = document.querySelector("#lock-landscape-button"),
-// status = document.querySelector("#orientation-status");
+let lockButton = document.querySelector("#lock-landscape-button"),
+    status = document.querySelector("#orientation-status");
 
-// status.innerHTML = screen.orientation.type + ' mode';
+status.innerHTML = screen.orientation.type + ' mode';
 
-// lockButton.addEventListener('click', function() {
-window.addEventListener("load", function(event) {
-    alert("All resources finished loading!$");
-    if (document.documentElement.requestFullscreen({ navigationUI: "show" }))
-        document.getElementById("container").requestFullscreen({ navigationUI: "show" });
+// upon lock to landscape-primary mode
+document.querySelector("#lock-landscape-button").addEventListener('click', function() {
+    if (document.documentElement.requestFullscreen)
+        document.querySelector("#container").requestFullscreen();
     else if (document.documentElement.webkitRequestFullScreen)
-        document.getElementById("container").webkitRequestFullScreen({ navigationUI: "show" });
+        document.querySelector("#container").webkitRequestFullScreen();
 
     screen.orientation.lock("landscape-primary")
         .then(function() {
-            status.innerHTML = screen.orientation.type + ' mode';
-            document.getElementById("container").style.backgroundColor = 'green';
+            // lockButton.style.display = 'none';
+            document.querySelector("#container").style.backgroundColor = 'green';
         })
         .catch(function(error) {
             alert(error);
         });
 });
-// });
