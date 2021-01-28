fee8e151f2c091ac59170584679cc388a7342c38
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-01-28 13:22:56 +0400

    add con.log

--- js/script.js
+++ js/script.js
@@ -5,16 +5,20 @@
 
 // lockButton.addEventListener('click', function() {
 async function setSreen() {
+    console.log(1)
     if (await document.documentElement.requestFullscreen)
         await document.querySelector(".container").requestFullscreen();
     else if (await document.documentElement.webkitRequestFullScreen)
         await document.querySelector(".container").webkitRequestFullScreen();
     // set landscape-primary
     screen.orientation.lock("landscape-primary")
-        .then(function() {})
+        .then(function() {
+            console.log(2)
+        })
         .catch(function(error) {
             alert(error);
         });
+    console.log(3)
 }
 
 setSreen();
