9bf9d628f18d02ea82e7d90f2db0933dfd4da020
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-01-28 13:15:40 +0400

    delete status

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // let lockButton = document.querySelector("#lock-landscape-button"),
-status = document.querySelector("#orientation-status");
+// status = document.querySelector("#orientation-status");
 
-status.innerHTML = screen.orientation.type + ' mode';
+// status.innerHTML = screen.orientation.type + ' mode';
 
 // lockButton.addEventListener('click', function() {
 if (document.documentElement.requestFullscreen)
