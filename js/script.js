5239bd910a09962ea9511e2ba0b49692f01a91fe
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-02-01 17:25:48 +0400

    sddddddddddd

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@ $(document).ready(function() {
 
     $('#ok').click(function() {
         if (document.documentElement.requestFullscreen)
-            await document.querySelector(".container").requestFullscreen();
+            document.querySelector("#container").requestFullscreen();
         else if (document.documentElement.webkitRequestFullScreen)
-            await document.querySelector(".container").webkitRequestFullScreen();
+            document.querySelector("#container").webkitRequestFullScreen();
 
         screen.orientation.lock("landscape-primary")
             .then(function() {
-                lockButton.style.display = 'none';
-                document.querySelector(".container").style.backgroundColor = 'green';
+                _LOCK_BUTTON.style.display = 'none';
+                _UNLOCK_BUTTON.style.display = 'block';
             })
             .catch(function(error) {
                 alert(error);
             });
     })
+
+    document.addEventListener('fullscreenchange', exitHandler);
+    document.addEventListener('webkitfullscreenchange', exitHandler);
+    document.addEventListener('mozfullscreenchange', exitHandler);
+    document.addEventListener('MSFullscreenChange', exitHandler);
+
+    function exitHandler() {
+        if (!document.fullscreenElement && !document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+            ///fire your event
+            alert(5);
+        }
+    }
 
 })
